e4a0dcc445a58e7f42eeb8dbde468fedd6863fcd
muhammadyasirabbas <145529970+MujtabaInfini8ai@users.noreply.github.com>
2023-09-28 17:33:44 +0500

    asS

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(
 const io = require("socket.io")(process.env.PORT || 3001, {
   cors: {
     // origin: "https://editor-frontend-nu.vercel.app",
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
